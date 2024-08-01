d5fd35f934158153319f75b2ba3d74b8e663f2ce
Alex B <alexborcan@meta.com>
2024-08-01 12:40:53 -0700

    [001] Add base

---
+++ 001_fcas_debug_lines/generated.cpp
@@ -0,0 +1,45 @@
+#include <cstdint>
+extern "C" int my_func_01(
+    int a,
+    int b)
+{
+    return
+      a
+      *
+      a
+      +
+      b;
+}
+extern "C" int my_func_02(
+    int a,
+    int b)
+{
+    return
+      a
+      *
+      a
+      +
+      b;
+}
+extern "C" int my_func_03(
+    int a,
+    int b)
+{
+    return
+      a
+      *
+      a
+      +
+      b;
+}
+extern "C" int my_func_04(
+    int a,
+    int b)
+{
+    return
+      a
+      *
+      a
+      +
+      b;
+}
